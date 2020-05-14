1018541afb500d604cab209cd62bcba6a392504a
Grzegorz Zygmański <gzygmanski@hotmail.com>
2020-05-14 21:04:36 +0000

    chore: update version

--- snr.py
+++ snr.py
@@ -14,7 +14,7 @@ from imports.screen import Screen, Pager
 
 # :::: APP INFO :::::::::::::::: #
 
-VERSION = 'v0.2.30-alpha'
+VERSION = 'v0.3.36-alpha'
 APP = 'Simple Novel Reader'
 
 # :::: KEYBINDINGS ::::::::::::: #
